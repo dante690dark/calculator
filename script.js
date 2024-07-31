b16dd713b8527eac8032ccf20b54f0bf7fb94ce3
Alexander Manns <dante690dark@gmail.com>
2024-07-30 23:04:29 -0500

    add number in display of the calculator

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
+let firsNumber, secondNumber, operator
 
 const sum = (a, b ) => a + b
 const subtract = (a, b ) => a - b
 const multiply = (a, b ) => a * b
 const divide = (a, b ) => a / b
+
+
+const operate = (num1, num2) => {
+  console.log(num1, num2)
+}
+
+operate(3, 4)
+
+//Events
+const display = document.querySelector('#display')
+const buttons = document.querySelectorAll('.buttons')
+
+const buttonsArray = [...buttons]
+
+buttonsArray.forEach(button => {
+  button.addEventListener('click', (e) => {
+    display.innerText += e.target.innerText
+    console.log( typeof +display)
+  });
+});
