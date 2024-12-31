63bc986ffe350aaf5946ade996122ed9c006c08a
Alexander Manns <dante690dark@gmail.com>
2024-12-31 09:48:04 -0500

    update the fixme

--- script.js
+++ script.js
@@ -149,3 +149,4 @@ document.addEventListener('keydown', (event) => {
 
 // FIXME: fix when you press an operator, then a number a finally and operator again, unexpected behavior
 // FIXME: add more styles to looks like apple current apple calculator
+// FIXME: change C button for x button, that letme to delete one element on the screen
